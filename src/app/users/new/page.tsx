d845216abaed8452707425d410ba8703f643f3e7
Nicolas <jnvillegas86gmail.com>
2025-07-22 00:10:54 -0300

    actulizacion frontend jul-2025

--- src/app/users/new/page.tsx
+++ src/app/users/new/page.tsx
@@ -12,7 +12,7 @@ import {
 import { Input } from "@/components/ui/input"
 import { Label } from "@/components/ui/label"
 
-export function CardDemo() {
+export default function CardDemo() {
   return (
     <Card className="w-full max-w-sm">
       <CardHeader>
